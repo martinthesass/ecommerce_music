5599844e9c59497b92da4230544c089faf506579
martin cosgrove <martin.cosgrove55@gmail.com>
2023-07-30 13:45:36 +0100

    commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,23 +1,21 @@
-import React from 'react'
-import {Toaster} from 'react-hot-toast'
+import React from "react";
+import { Toaster } from "react-hot-toast";
 
-import { Layout } from '../components'
+import { Layout } from "../components";
 
-import '../styles/globals.css'
+import "../styles/globals.css";
 
-import {StateContext} from '../context/StateContext'
-
+import { StateContext } from "../context/StateContext";
 
 function MyApp({ Component, pageProps }) {
-
   return (
     <StateContext>
-  <Layout>
-    <Toaster /> 
-   <Component {...pageProps} />
-  </Layout >
-  </StateContext>
-  )
+      <Layout>
+        <Toaster />
+        <Component {...pageProps} />
+      </Layout>
+    </StateContext>
+  );
 }
 
-export default MyApp
+export default MyApp;
