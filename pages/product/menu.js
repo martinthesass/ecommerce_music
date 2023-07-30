16b2a653359afeced01dcd75191c702490b20e19
martin cosgrove <martin.cosgrove55@gmail.com>
2023-07-30 13:37:09 +0100

    change

--- pages/product/menu.js
+++ pages/product/menu.js
@@ -1,18 +1,18 @@
-import React from "react";
+// import React from "react";
 
-const Menu = ({Product}) => {
-    return <div className="section-center">
+// const Menu = ({Product}) => {
+//     return <div className="section-center">
 
-        {Product.map((menuItem)=>{
+//         {Product.map((menuItem)=>{
 
-            const {id, image, name, slug, price} = menuItem;
+//             const {id, image, name, slug, price} = menuItem;
 
-            return <article key={id} className=''>
+//             return <article key={id} className=''>
 
-                <img src={image} alt="" />
-            </article>
+//                 <img src={image} alt="" />
+//             </article>
 
-        })}
+//         })}
 
-    </div>
-}
+//     </div>
+// }
